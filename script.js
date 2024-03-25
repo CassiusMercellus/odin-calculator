69896327124f8a0d41066e050bf82b330ed3618b
Cassius Hill <MarcelusWork@gmail.com>
2024-03-25 10:21:45 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMcontentLoaded', function() {
+    const ac = document.getElementById("AC");
+    const zero = document.getElementById("0");
+    const one = document.getElementById("1");
+    const two = document.getElementById("2");
+    const three = document.getElementById("3");
+    const four = document.getElementById("4");
+    const five = document.getElementById("5");
+    const six = document.getElementById("6");
+    const seven = document.getElementById("7");
+    const eight = document.getElementById("8");
+    const nine = document.getElementById("9");
+    const addition = document.getElementById("addition");
+    const subtraction = document.getElementById("subtraction");
+    const multiplication = document.getElementById("multiplication");
+    const division = document.getElementById("division");
+    const equal = document.getElementById("equal");
+    const decimal = document.getElementById("decimal");
+})
